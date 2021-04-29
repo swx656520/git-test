3724a3d0b90759777b35268e0894122e91b0fb26
swx656520 <swx656520@tom.com>
2021-04-29 17:56:08 +0800

    master commit 2

--- src/main/java/com/song/gitdemo/GitTest.java
+++ src/main/java/com/song/gitdemo/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("Hello git");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
-        System.out.println("Hello git3");
+        System.out.println("Hello git4");
+        System.out.println("master test");
     }
 }
