6ac6d3b7adf8a3b4dce12c7915fd52a9bfc4eee2
swx656520 <swx656520@tom.com>
2021-04-29 15:03:04 +0800

    git-demo test by idea first

---
+++ src/main/java/com/song/gitdemo/GitTest.java
@@ -0,0 +1,13 @@
+package com.song.gitdemo;
+
+/**
+ * @author song ning
+ * @create 2021-04-29 14:50
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("Hello git");
+    }
+}
